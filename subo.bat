1ea46297cf9a6e62e3be7145ad63506950759966
Matu36 <matipineda85@live.com.ar>
2023-10-16 01:34:45 -0300

    Subiendo a lambda

--- subo.bat
+++ subo.bat
@@ -1,6 +1,9 @@
 git add .
 git commit -m "Subiendo a lambda"
 git push
-go build main.go
+go build -o main.go
 del main.zip      
-tar.exe -a -cf main.zip main 
+tar.exe -a -cf zip main.zip main 
+
+
+
