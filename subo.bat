3a164f6b896a8850ec1c99a15f6ed48cf5bea7bb
Matu36 <matipineda85@live.com.ar>
2023-12-25 13:26:46 -0300

    Subiendo a lambda

--- subo.bat
+++ subo.bat
@@ -1,7 +1,8 @@
 git add .
 git commit -m "Subiendo a lambda"
 git push
-GOARCH=amd64 GOOS=linux go build -a -o ./build/main main.go
+GOOS=linux GOARCH=amd64 go build -o main main.go
+
 @REM go build -o main main.go
 del main.zip      
 tar.exe -a -cf main.zip main 
